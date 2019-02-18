5bcb25037dcf5bc481d98083783413878cf5c9f6
gs369 <gahir2221@gmail.com>
2019-02-18 11:00:29 +0000

    define

--- main.c
+++ main.c
@@ -1,18 +1,28 @@
+/*PROGRAM FOR RASPBERRYPI MODEL 3B+*/
+/*PROGRAM BY GURSIMRAN SINGH,gs369*/
+/*PROGRAM SOURCE - https://www.github.com/gs369/mushroompi*/
+
+/*LIBRARY*/
 #include<stdio.h>
 #include<wiringPi.h>
 #include<string.h>
-
+/*DECLARAION OF RASPBERRYPI PINS*/
 #define LED 17          //LED
 #define RELAY 27	//Moisture Pump
 #define RELAY2 22	//Cooling Pump
 #define RELAY3 10	//Nutrient Pump
 #define RELAY4 9 	//Heater
+#define SOIL_S 11	//SOIL SENSOR
+#define TEMP_S 0	//TEMPERATURE SENSOR
+#define LIGHT_S 5	//LIGHT SENSOR(LDR)
+#define ION_S 6 	//NUTRIENT SENSOR(ION)
+/*FOR LINES*/
 char *dash(char *buf, int len ) {
    memset(buf, '-', len);
    buf[len] = 0;
    return buf;
 }
-
+/*DISPLAY - PROJECT,NAME,STUDENT_ID,INSTRUCTORS*/
 int main (void)
 {
 	char buf[60];
@@ -27,15 +37,21 @@ int main (void)
 	printf("%s\n", dash(buf,60));
 	printf("INSTRUCTORS : PROF. TAKIS ZOURNTOS\n\t      PROF. MIKE ALESHAMS\n");
 	printf("%s\n", dash(buf,60));
-
+	/*GPIO*/
 	wiringPiSetupGpio();
 	/*WiringPi GPIO*/
-	pinMode(LED,OUTPUT);		//LED
+	/*DECLARATION OF OUTPUTS*/
+	pinMode(LED,OUTPUT);	//LED
 	pinMode(RELAY,OUTPUT);	//Moisture Pump
 	pinMode(RELAY2,OUTPUT);	//Cooling Pump
 	pinMode(RELAY3,OUTPUT);	//Nutrient Pump
 	pinMode(RELAY4,OUTPUT);	//Heater
-
+	/*DECLARATION OF INPUTS*/
+	pinMode(SOIL_S,INPUT);
+	pinMode(TEMP_S,INPUT);
+	pinMode(LIGHT_S,INPUT);
+	pinMode(ION_S,INPUT);
+	/*digitalWrite fn_OUTPUTS*/
 	digitalWrite(RELAY,0);
 	digitalWrite(RELAY2,0);
 	digitalWrite(RELAY3,0);
@@ -49,5 +65,7 @@ int main (void)
 		 digitalWrite(LED, 0);
 		 delay(500);
 		}
+return 0;
 }
+
 
