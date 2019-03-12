ad82ee228f0a2a084fae616265497f6675152c97
gs369 <gahir2221@gmail.com>
2019-03-12 10:24:57 +0000

    addition

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ $ sudo nano /boot/cmdline.txt
 	Original Text -> dwc_otg.1pm_enable=0 console=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p6 ro.............
 $ sudo reboot
 *********************************************************
+
+Reference: https://www.abelectronics.co.uk/kb/article/1035/raspberry-pi-3-and-zero-w-serial-port-usage
--- receive.c
+++ receive.c
@@ -8,7 +8,7 @@ int main ()
 {
   int fd ;
 
-  if ((fd = serialOpen ("/dev/ttyAMA0", 9600)) < 0)
+  if ((fd = serialOpen ("/dev/ttyAMA0", 115200)) < 0)
   {
     fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
     return 1 ;
@@ -18,7 +18,9 @@ int main ()
 
   for (;;)
   {
-    putchar (serialGetchar (fd)) ;
-    fflush (stdout) ;
+
+    putchar (serialGetchar(fd));
+    fflush (stdout);
+
   }
 }
